f983c3ff77ff91dea125a4095c851d0224365f4e
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-09-16 22:30:23 -0300

    Adicionando compomente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hellow Semana DevSuperior!</h1>
+    
+    <div>
+      <Header />
+    </div>
   );
 }
 
