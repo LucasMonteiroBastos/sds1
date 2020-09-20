6e71a000a273da7f6fd3a2dff7fc0ea7b8a55bfe
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-09-20 10:36:08 -0300

    Adicionando rotas no React Native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -10,7 +10,7 @@ import {
 
 
 import Header from './src/components/Header';
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
 
@@ -24,8 +24,7 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <Header />
-        <Home />
+        <Routes />
         <StatusBar style="light" />
       </View>
     );
@@ -35,6 +34,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0B1F34',
   },
 });
--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { FontAwesome5 as Icon } from '@expo/vector-icons'
 import { Text, StyleSheet, View, Image, Alert } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../../components/Header';
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
 
+    const navigation = useNavigation();
+
     const handlerOnPress = () => {
-        Alert.alert('Voce clicou no butão')
+      navigation.navigate('CreateRecord');
     }
 
     return(
        <>
+       <Header />
        <View style={styles.container}>
            <Image 
                 source={require('../../assets/gamer.png')} 
